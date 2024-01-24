7a1a0e6dbf3c22c240ea54633c5cb493604be46e
DoudGaya <arbdoule@yahoo.com>
2024-01-24 10:51:40 +0100

    landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import { Navigation } from "@/components/Navigation";
 import Image from "next/image";
 import Link from "next/link";
+import bannerImage from '@/app/img/banner-image-light.svg'
+import bannerImageDark from '@/app/img/banner-image-dark.svg'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { LoginForm } from "@/components/RegisterForm";
 
 export default function Home() {
   return (
@@ -9,15 +20,35 @@ export default function Home() {
     <Navigation />
       <div className=" grid grid-cols-2 h-[70%] mx-auto max-w-6xl gap-x-2">
             <div className="flex space-y-8 flex-col my-auto">
-              <h1 className=" text-5xl font-bold "> <p className="text-blue-500">Get Verified.</p>  <p>Take a Screenshot & Share with your friends</p> </h1>
-           <div className=" my-4">
-           <Link href={''} className=" font-semibold bg-blue-500 rounded-full px-6 py-2">
-              Get Started for free
-            </Link>
-           </div>
+                <h1 className=" text-5xl font-bold "> <p className="text-blue-500">Get Verified.</p>  <p>Take a Screenshot & Share with your friends</p> </h1>
+                <div className=" my-4">
+                      <Dialog>
+                      <DialogTrigger>
+                        <div className=" font-semibold bg-blue-500 rounded-full px-6 py-2">
+                          Get Started for free
+                        </div>
+                      </DialogTrigger>
+                     <div className="">
+                      <DialogContent className="">
+                          <DialogHeader>
+                            <DialogTitle className=" my-6 flex justify-center">
+                              <div className=" text-xl">Create an Account</div>
+                            </DialogTitle>
+                            <DialogDescription>
+                                <LoginForm />
+                            </DialogDescription>
+                          </DialogHeader>
+                        </DialogContent>
+                     </div>
+                    </Dialog>
+
+                </div>
             </div>
-          </div>
-          <div className=""></div>
+            <div className="">
+             <Image src={bannerImage} className=" block dark:hidden h-[500px] " alt="" width={1000} height={1000} />
+             <Image src={bannerImageDark} className=" hidden dark:block h-[500px] " alt="" width={1000} height={1000} />
+            </div>
+      </div>
     </div>
     </main>
   );
---
+++ components/RegisterForm.tsx
@@ -0,0 +1,149 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  firstName: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  lastName: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  passwordConfirmation: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+
+
+
+export function LoginForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+
+      <div className="grid grid-cols-2 gap-2">
+      <FormField
+          control={form.control}
+          name="firstName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Firs tName</FormLabel>
+              <FormControl>
+                <Input placeholder="First Name" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+         <FormField
+          control={form.control}
+          name="lastName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>LastName</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+       <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input className=" focus:outline-blue-600" placeholder="Username" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+         <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+       <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+         <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </div>
+
+       <div className=" flex w-full justify-center">
+        <Button  type="submit" className=" px-6">Submit</Button>
+       </div>
+      </form>
+    </Form>
+  )
+}
