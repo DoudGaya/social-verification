3b7efb7ec8fd77a14960ffe13d1786a703b29e76
doudgaya <arbdoule@yahoo.com>
2024-04-08 13:51:30 +0100

    auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,43 +1,7 @@
-import { connectMongoDB } from "@/lib/mongodb";
-import User from "@/models/user";
+
 import NextAuth from "next-auth/next";
-import bcrypt from "bcryptjs"
-import  CredentialsProvider from "next-auth/providers/credentials";
-
- const authOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "credentials",
-            credentials: {},
-            async authorize( credentials) {
-                // @ts-ignore
-                const {email, password} = credentials
-
-                try {
-                    await connectMongoDB()
-                    const user = await User.findOne({email})
-                    if (!user) return null
-                    const passwordCheck = await bcrypt.compare(password, user.password)
-                    if (!passwordCheck) return null
-                    return user
-                } catch (error) {
-                    console.log("Error:", error) 
-                }
-               
-            }
-        })
-    ],
-    session: {
-        strategy: "jwt",
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    pages: {
-        signIn: "/"
-    }
-}
+import { authOptions } from "@/lib/authOptions";
 
 // @ts-ignore
 const handler = NextAuth(authOptions)
-
-
 export {handler as GET, handler as POST, authOptions}
